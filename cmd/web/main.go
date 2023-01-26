4eb92fded6572c91f46454ce2e912c8a400641a5
Mike <nullsnull84@gmail.com>
2023-01-26 14:23:33 +0400

    applied static files successfully

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/post", showPost)
+	mux.HandleFunc("/post/create", createPost)
 
-	http.HandleFunc("/", home)
-	http.HandleFunc("/post", showPost)
-	http.HandleFunc("/post/create", createPost)
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", nil)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
